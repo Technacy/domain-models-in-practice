584bf9b084561b5446551701f22c82e884598ce6
paolominel <paolo.minel@technacy.it>
2022-01-21 15:45:50 +0100

    secondo commit

--- test/examples/tennis.test.ts
+++ test/examples/tennis.test.ts
@@ -1,13 +1,36 @@
 import { expect } from "chai"
 
+class Game {
+  score1: string = "love"
+  score2: string = "love"
+
+  addPointToPlayer1() {
+    this.score1 = "15"
+  }
+
+  score() {
+    return `${this.score1}-${this.score2}`
+  }
+}
 
 describe("Tennis", () => {
+  it("Game has `love-love` at beginning", () => {
+    const game = new Game() // arrange
 
-    it("Game has `love-love` at beginning", () => {
-        const game = new Game(); // arrange
+    const score = game.score() // act
 
-        const score = game.score(); // act
+    expect(score).eql("love-love") // assert
+  })
 
-        expect(score).eql("love-love"); // assert
-    })
+  it("If player 1 make a point score must be 15-love", () => {
+     // arrange
+    const game = new Game()
+
+    // act
+    game.addPointToPlayer1()
+    
+    // assert
+    const score = game.score()
+    expect(score).eql("15-love")
+  })
 })
