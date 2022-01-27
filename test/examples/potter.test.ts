dec1aa10aa3a89f8c7e26e45cd8a0ec4d867ff5c
francescocecchi <francesco.cecchi@technacy.it>
2022-01-27 17:14:50 +0100

    5 different books

--- test/examples/potter.test.ts
+++ test/examples/potter.test.ts
@@ -4,10 +4,10 @@ type BookTitle = "b1" | "b2" | "b3" | "b4" | "b5"
 
 function calcAmount(cart: BookTitle[]): number {
   if (cart.every((el) => el === cart[0])) return 8 * cart.length
-  if (cart.length === 3) {
-    return 21.6
-  }
-  return 15.2
+
+  const discount = [0, 0.95, 0.9, 0.8, 0.75]
+
+  return 8 * cart.length * discount[cart.length - 1]
 }
 
 describe(`Calculate amount`, () => {
@@ -41,5 +41,29 @@ describe(`Calculate amount`, () => {
     const amount = calcAmount(cart)
 
     expect(amount).eq((8 + 8 + 8) * 0.9)
+  })
+
+  it(`buy 4 different  books -> amount must be (8 + 8 + 8 + 8) - 20%`, () => {
+    const cart: BookTitle[] = ["b1", "b2", "b3", "b4"]
+
+    const amount = calcAmount(cart)
+
+    expect(amount).eq((8 + 8 + 8 + 8) * 0.8)
+  })
+
+  it(`buy 5 different  books -> amount must be (8 + 8 + 8 + 8 + 8) - 25%`, () => {
+    const cart: BookTitle[] = ["b1", "b2", "b3", "b4", "b5"]
+
+    const amount = calcAmount(cart)
+
+    expect(amount).eq((8 + 8 + 8 + 8 + 8) * 0.75)
+  })
+
+  it(`buy 0 books -> 0 as result`, () => {
+    const cart: BookTitle[] = []
+
+    const amount = calcAmount(cart)
+
+    expect(amount).eq(0)
   })
 })
