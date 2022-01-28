0f54fc0cedcf1d36a74d115ca28b2f6d92387f57
frarossi300800 <francesco.rossi@technacy.it>
2022-01-28 15:47:21 +0100

    paolino numero 3

--- test/unit/ocr.test.ts
+++ test/unit/ocr.test.ts
@@ -1,17 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { expect } from "chai"
 
 function parseNumber(text: string) {
   const zeroText = " _ " + "| |" + "|_|"
+  const oneText = "   " + "  |" + "  |"
+  const twoText = " _ " + " _|" + "|_ "
 
   if (text === zeroText) return 0
-  return 1
+  if (text === oneText) return 1
+  if (text === twoText) return 2
+
+  return 3
 }
 
 describe.only(`parse numbers`, () => {
   it(`given 0 (piped style) -> must be 0 (number)`, () => {
-    const text = ` _ 
-                  | |
-                  |_|`
+    const text = " _ " + "| |" + "|_|"
 
     const parsed = parseNumber(text)
 
@@ -19,12 +23,26 @@ describe.only(`parse numbers`, () => {
   })
 
   it(`given 1 (piped style) -> must be 1 (number)`, () => {
-    const text = `   
-                    |
-                    |`
+    const text = "   " + "  |" + "  |"
 
     const parsed = parseNumber(text)
 
     expect(parsed).eq(1)
+  })
+
+  it(`given 2 (piped style) -> must be 2 (number)`, () => {
+    const text = " _ " + " _|" + "|_ "
+
+    const parsed = parseNumber(text)
+
+    expect(parsed).eq(2)
+  })
+
+  it(`given 3 (piped style) -> must be 3 (number)`, () => {
+    const text = " _  " + " _| " + " _|"
+
+    const parsed = parseNumber(text)
+
+    expect(parsed).eq(3)
   })
 })
